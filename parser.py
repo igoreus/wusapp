c657118e44167527f71c66c727dcf0d9972099e7
igor <igor.veremchuk@gmail.com>
2012-10-15 21:09:42 +0300

    implemented task 2

--- parser.py
+++ parser.py
@@ -1,4 +1,17 @@
 from xml.dom import minidom
-f = open('fixtures/input.html')
+from sys import stdout
+
+f = open('fixtures/input.html', mode='r')
 dom = minidom.parse(f)
+
+def printDom(domElement):
+
+    for node in domElement.childNodes:
+
+        if isinstance(node, minidom.Element):
+            stdout.write("%s -> %s\n" % \
+                  (node.nodeName ,' '.join([i.nodeName for i in node.childNodes if isinstance(i, minidom.Element)])))
+            printDom(node)
+
+printDom(dom)
 
